6438f154fea9d1e004c2dbba43a3e66ec5213ba6
Patitobi <100159085+Patitobi@users.noreply.github.com>
2022-09-06 09:39:23 +0200

    x

--- ToDo.py
+++ ToDo.py
@@ -224,6 +224,42 @@ def quary_json_data(jahr=None, liste_tag_namen=None, liste_monate=None):
         if ero != None:
             print(ero)
 
+############################# RESETET ALLES ###############################################
+def reset():
+    with open("ToDO\JSON\kalender_data.json", "r") as data_file:
+        main={}
+        data = json.load(data_file)
+        tage_monat=list()
+        for x in range(12):
+            tage_monat.append(int(data.get("list_monate")[x].get("anz_days")))
+    
+    with open("ToDO\JSON\Termine_data.json", "w") as data_file:
+        for monate in range(12):
+            main.update({str(monate):{}})
+            for tage in range(tage_monat[monate]):
+                main.get(str(monate)).update({str(tage):{}})
+        json.dump(main, data_file, indent=4)
+
+def append_termin(monat=int(), tag=int(), name=str(), von=None, bis=None, text=None, fabe=None, id=None):
+    with open("JSON\Termine_data.json", "r") as data_file:
+        data = json.load(data_file)
+        if id == None:
+            id = data.get("data").get("id") + 1
+            data.get("data").pop("id")
+            data.get("data").update({"id":id})
+        data.get(str(monat)).get(str(tag)).update(
+                    {f"{id}":{
+                    "name":name,
+                    "von":von,
+                    "bis":bis,
+                    "text":text,
+                    "fabe":fabe,
+                    "id":id
+                    }}
+                )
+    with open("JSON\Termine_data.json", "w") as data_file:
+        json.dump(data, data_file, indent=4)
+
 if __name__=="__main__":
     ref_jahr = class_jahr(name=quary_json_data(jahr="name"))
     gui=True
